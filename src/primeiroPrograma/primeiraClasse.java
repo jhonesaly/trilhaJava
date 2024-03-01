171d23ad9f153fd012a2fd44301e11982b024148
Jhones <jhonesaly@gmail.com>
2024-03-01 15:58:42 -0300

    Operadores

---
+++ src/primeiroPrograma/primeiraClasse.java
@@ -0,0 +1,7 @@
+package primeiroPrograma;
+
+public class primeiraClasse {
+    public static void main(String[] args) {
+        System.out.printf("Concluí a aula 01 e agora estou mergulhando em Java!");
+    }
+}
