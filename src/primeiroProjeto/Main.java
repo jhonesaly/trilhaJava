eb120db79d8dfb187d037d11d4024ea7a9e15397
Jhones <jhonesaly@gmail.com>
2024-03-01 16:28:59 -0300

    Tipos primitivos

--- src/primeiroProjeto/Main.java
+++ src/primeiroProjeto/Main.java
@@ -33,10 +33,17 @@ public class Main {
             - Nome de variável: minhaVariavel
             - Linhas de código devem ter menos de 80 char
             - Espaços para separar operadores, palavras-chave e elementos de controle de fluxo | ex: if (condicao) {
-            - Use comentários para documentar seu código (//linha ou /*bloco)
+            - Use comentários para documentar seu código (//linha)
 
-            Tipos de variáveis:
-                -
+          Tipos primitivos:
+            - boolean (true ou false)
+            - char (caracteres unicode individuais ex:'a')
+            - byte (inteiros de 8 bits [-128 a 127])
+            - short (inteiros de 16 bits [-32.768 a 32.767])
+            - int (inteiros de 32 bits [-2.147.483.648 a 2.147.483.647])
+            - long (inteiros de 64 bits [-9.223.372.036.854.775.808 a 9.223.372.036.854.775.807 {+-9.2*10^18}])
+            - float (reais de 32 bits)
+            - double (reais de 64 bits)
          */
 
     }
