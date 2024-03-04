d354706e5d5ee842a5b1edf53cbcd15c1959a433
Jhones <jhonesaly@gmail.com>
2024-03-04 11:17:12 -0300

    feat: Q5

--- src/Pratica2/Main.java
+++ src/Pratica2/Main.java
@@ -65,5 +65,16 @@ public class Main {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i * numero1);    
         }
+
+        /*5) Crie um programa que solicite ao usuário a entrada de um número inteiro. 
+        Verifique se o número é par ou ímpar e exiba uma mensagem correspondente.
+         */
+        System.out.print("\n5) Digite um número: ");
+        numero1 = scanner.nextInt();
+        if (numero1%2 == 0) {
+            System.out.println("O número é par.");
+        } else {
+            System.out.println("O número é ímpar.");
+        }
     }
 }
