c0aeb03302adba0d87176605cfa44805df857437
Jhones <jhonesaly@gmail.com>
2024-03-04 16:37:21 -0300

    feat: separando classes

--- src/Projeto1/Main.java
+++ src/Projeto1/Main.java
@@ -81,30 +81,3 @@ public class Main {
 
     }
 }
-
-class Usuario {
-
-    private String nome;
-    private double saldo;
-
-    public Usuario(String nome, double saldo) {
-        this.nome = nome;
-        this.saldo = saldo;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-}
---
+++ src/Projeto1/Usuario.java
@@ -0,0 +1,29 @@
+package Projeto1;
+
+public class Usuario {
+
+    private String nome;
+    private double saldo;
+
+    public Usuario(String nome, double saldo) {
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
+
