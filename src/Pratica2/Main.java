409a763fd2a42cc466e19b05cad400e4d6493c5f
Jhones <jhonesaly@gmail.com>
2024-03-04 11:13:27 -0300

    feat: Q4

--- src/Pratica2/Main.java
+++ src/Pratica2/Main.java
@@ -32,7 +32,7 @@ public class Main {
 
         //2:Peça ao usuário para inserir dois números inteiros. 
         //.1:Compare os números e imprima uma mensagem indicando se são iguais, diferentes, o primeiro é maior ou o segundo é maior.
-        System.out.print("2) Digite o primeiro número: ");
+        System.out.print("\n2) Digite o primeiro número: ");
         numero1 = scanner.nextInt();
         System.out.print("2) Digite o segundo número: ");
         numero2 = scanner.nextInt();
@@ -47,7 +47,7 @@ public class Main {
         /*Crie um menu que oferece duas opções ao usuário: 
         "1. Calcular área do quadrado" e "2. Calcular área do círculo". 
         Solicite a escolha do usuário e realize o cálculo da área com base na opção selecionada. */
-        System.out.print("3) Digite um número: ");
+        System.out.print("\n3) Digite um número: ");
         numero1 = scanner.nextInt();
         System.out.print("3) Escolha uma das opções:\n[1] Calcular área do quadrado\n[2] Calcular área do círculo\n");
         numero2 = scanner.nextInt();
@@ -56,6 +56,14 @@ public class Main {
         } else {
             numero3 = Math.PI*numero1*numero1;
         }
-        System.out.printf("A área calculada é: %f", numero3);
+        System.out.printf("A área calculada é: %f\n", numero3);
+
+        /*4) Crie um programa que solicite ao usuário um número e exiba a tabuada desse número de 1 a 10.*/
+        System.out.print("\n4) Digite um número: ");
+        numero1 = scanner.nextInt();
+        System.out.println("Sua tabuada é: ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i * numero1);    
+        }
     }
 }
