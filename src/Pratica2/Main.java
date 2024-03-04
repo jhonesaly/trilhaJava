21a541f4aadbbb09a2ddfee0c7c86978c92609ad
Jhones <jhonesaly@gmail.com>
2024-03-04 10:32:33 -0300

    feat: Q2

--- src/Pratica2/Main.java
+++ src/Pratica2/Main.java
@@ -18,15 +18,27 @@ public class Main {
         //.1: Se o número for positivo, exiba "Número positivo", caso contrário, exiba "Número negativo"
 
         System.out.print("1) Digite um número: ");
-        int numero = scanner.nextInt();
-        if (numero > 0) {
+        int numero1 = scanner.nextInt();
+        if (numero1 > 0) {
             System.out.println("O número é positivo.");
-        } else if (numero < 0) {
+        } else if (numero1 < 0) {
             System.out.println("O número é negativo.");
         } else {
             System.out.println("O número é nulo.");
         }
 
-
+        //2:Peça ao usuário para inserir dois números inteiros. 
+        //.1:Compare os números e imprima uma mensagem indicando se são iguais, diferentes, o primeiro é maior ou o segundo é maior.
+        System.out.print("2) Digite o primeiro número: ");
+        numero1 = scanner.nextInt();
+        System.out.print("2) Digite o segundo número: ");
+        int numero2 = scanner.nextInt();
+        if (numero1 == numero2) {
+            System.out.println("Os números são iguais.");
+        } else if (numero1 > numero2) {
+            System.out.println("O primeiro número é maior que o segundo.");
+        } else {
+            System.out.println("O primeiro número é menor que o segundo.");
+        }
     }
 }
