c5322a982c76e7e83e9ac01830b2ab4393fb622e
Jhones <jhonesaly@gmail.com>
2024-03-05 10:51:05 -0300

    feat: P4Q2

--- src/Pratica4/Principal.java
+++ src/Pratica4/Principal.java
@@ -1,6 +1,7 @@
 package Pratica4;
 
 import Pratica4.Classes.ContaBancaria;
+import Pratica4.Classes.IdadePessoa;
 
 public class Principal {
     public static void main(String[] args) {
@@ -11,7 +12,17 @@ public class Principal {
         minhaConta.titular = "Alyson";
         System.out.println("A conta " + minhaConta.getNumeroConta() + " Possui R$" + minhaConta.getSaldo());    
     
-        
+        //2
+        IdadePessoa pessoa = new IdadePessoa();
+        pessoa.setNome("Paulo");
+        pessoa.setIdade(19);
+        System.out.println(
+            "O " 
+            + pessoa.getNome() + 
+            " Tem " 
+            + pessoa.getIdade() + 
+            " anos."
+            );
     
     }
 }
